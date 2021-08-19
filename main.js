84c8b81f97e429018c640d501aa5b12a2e98af20
Temi-Pinheiro <temitopepinheiro@gmail.com>
2021-08-19 15:00:53 +0100

    Updated js this time

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-console.log('Hello world');
+console.log('My name is Temitope Pinheiro');
 
 document.querySelector('#app').innerHTML = `
   <h1>Please inspect your console to see printed message</h1>
